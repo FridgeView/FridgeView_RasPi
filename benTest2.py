4f21147f7b54bcdee5a6682812e26ec5883462b5
kkasper <kkasper@bu.edu>
2016-11-12 20:51:37 -0500

    Full commit

--- benTest2.py
+++ benTest2.py
@@ -1,38 +1,29 @@
 import os
 os.environ["PARSE_API_ROOT"] = "https://fridgeview.herokuapp.com/parse"
-
+import base64
+import requests
+import picamera
 from parse_rest.datatypes import Function, Object, GeoPoint
 from parse_rest.connection import register
 from parse_rest.query import QueryResourceDoesNotExist
 from parse_rest.connection import ParseBatcher
 from parse_rest.core import ResourceRequestBadRequest, ParseError
+from parse_rest.user import User
+from parse_rest.datatypes import Object
 
 APPLICATION_ID = "FVAPPID123456789bcdjk"
 REST_API_KEY = "A"
 MASTER_KEY = "FVMASTERKEY123456789bcdjk"
 
 register(APPLICATION_ID, REST_API_KEY, master_key=MASTER_KEY)
+#This is not good
+U = User.login("fvAdmin","abcd")
 
-from parse_rest.datatypes import Object
-
-myClassName = "TestObject"
-myClass = Object.factory(myClassName)
-
-print myClass
-print myClass.__name__
-
-
-import base64
-import requests
-import picamera
+photoClassName = "Photos"
+photoClass = Object.factory(photoClassName)
 
 camera = picamera.PiCamera()
+encoded_string = base64.b64encode(camera.capture('newPhoto.jpg'))
 
-camera.capture('/home/pi/Desktop/image.jpg')
-
-with open("/home/pi/Desktop/image.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-
-
-dummyObject = myClass(foo1 = encoded_string)
-dummyObject.save()
+newPhoto = photoClass(encrypStr = encoded_string,device = 0,user=User.current_user())
+newPhoto.save()
